0401992f3db9935b59bc2e4ccff9623573825924
smetdenis <admin@jbzoo.com>
2016-08-04 20:06:18 +0700

    add start()

--- src/CovCatcher.php
+++ src/CovCatcher.php
@@ -144,7 +144,7 @@ class CovCatcher
     {
         if (!$this->_isStart && $this->_coverage) {
             $this->_isStart = true;
-            //$this->_coverage->start($this->_hash, true);
+            $this->_coverage->start($this->_hash, true);
         }
     }
 
