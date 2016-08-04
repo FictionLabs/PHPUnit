97e11f097fb665f218d6c06e3ea717315c81d033
smetdenis <admin@jbzoo.com>
2016-08-04 16:44:47 +0700

    add debug lines

--- fakedir/fake-index.php
+++ fakedir/fake-index.php
@@ -15,17 +15,25 @@
 
 // To help the built-in PHP dev server, check if the request was actually for
 // something which should probably be served as a static file
+echo __LINE__ . PHP_EOL;
 use JBZoo\PHPUnit\CovCatcher;
 
+echo __LINE__ . PHP_EOL;
 if (PHP_SAPI == 'cli-server') {
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = realpath($_SERVER['DOCUMENT_ROOT'] . $url['path']);
+    echo __LINE__ . PHP_EOL;
     if (is_file($file)) {
+        echo __LINE__ . PHP_EOL;
         return false;
     }
 }
 
+echo __LINE__ . PHP_EOL;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+echo __LINE__ . PHP_EOL;
 
 $catcher = new CovCatcher('test', array(
     'src'  => __DIR__ . '/../public_html',
@@ -34,4 +42,6 @@ $catcher = new CovCatcher('test', array(
     'cov'  => true,
 ));
 
+echo __LINE__ . PHP_EOL;
 $catcher->includeFile(__DIR__ . '/../public_html/index.php');
+echo __LINE__ . PHP_EOL;
